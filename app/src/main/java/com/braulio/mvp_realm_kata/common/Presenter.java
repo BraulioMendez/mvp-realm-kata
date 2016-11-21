2495851d9d269f922fba871d1eb72cd9ed492af0
BraulioMendez <braauliomendez@ciencias.unam.mx>
2016-11-21 00:31:18 -0600

    NEW: Added Basic Functions In App.

---
+++ app/src/main/java/com/braulio/mvp_realm_kata/common/Presenter.java
@@ -0,0 +1,10 @@
+package com.braulio.mvp_realm_kata.common;
+
+/**
+ * Created by Braulio on 20/11/2016.
+ */
+
+public interface Presenter {
+
+    void start();
+}
---
+++ app/src/main/java/com/braulio/mvp_realm_kata/view/ContactView.java
@@ -0,0 +1,12 @@
+package com.braulio.mvp_realm_kata.view;
+
+import com.braulio.mvp_realm_kata.common.View;
+
+/**
+ * Created by Braulio on 21/11/2016.
+ */
+
+public interface ContactView extends View {
+
+    void viewDetailContact();
+}
