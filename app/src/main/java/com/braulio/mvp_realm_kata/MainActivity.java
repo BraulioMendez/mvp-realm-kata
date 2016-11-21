2fa61171c54fdf685938d7b2da18841e4d78ce89
BraulioMendez <braauliomendez@ciencias.unam.mx>
2016-11-20 23:23:51 -0600

    Initial Commit.

---
+++ app/src/main/java/com/braulio/mvp_realm_kata/MainActivity.java
@@ -0,0 +1,13 @@
+package com.braulio.mvp_realm_kata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
